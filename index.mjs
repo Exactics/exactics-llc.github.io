1fb999c6d94ee7e78be6993f110461af721fb33e
pkage <patrick.r.kage@gmail.com>
2022-01-08 01:49:51 -0500

    upload new site

---
+++ index.mjs
@@ -0,0 +1,277 @@
+import * as THREE from './lib/three.module.js'
+
+const BALL_COUNT = 50
+const SHINY_COUNT = 4
+const CAMERA_MOVE_MODIFIER = 0.1
+const CARTON_MOVE_MODIFIER = 0.5
+
+class CanvasDriver {
+    constructor() {
+
+        this.carton = new CartonDriver()
+        // mouse setup
+        this.mouse = {x: 0.5, y: 0.5}
+
+        this.windowHalfX = window.innerWidth / 2;
+        this.windowHalfY = window.innerHeight / 2;
+
+        this.canvas = document.querySelector('canvas#c')
+
+
+        this.camera = new THREE.PerspectiveCamera( 20, window.innerWidth / window.innerHeight, 1, 10000 )
+        this.camera.position.z = 1800
+
+        this.scene = new THREE.Scene()
+        this.balls = new THREE.Object3D()
+        // this.scene.background = new THREE.Color( 0xf3e8ee )
+        this.scene.background = new THREE.Color( 0xe8e9f3 )
+
+        const light = new THREE.DirectionalLight( 0xffffff );
+        light.position.set( 0, 0, 1 );
+        this.scene.add( light );
+
+        
+        const radius = 25;
+
+        const geometry1 = new THREE.IcosahedronGeometry( radius, 1 );
+
+        const material = new THREE.MeshPhongMaterial( {
+            color: 0xbacdb0,
+            flatShading: true,
+            vertexColors: true,
+            shininess: 1
+        } );
+
+        const wireframeMaterial = new THREE.MeshBasicMaterial( { color: 0x000000, wireframe: true, transparent: true } );
+
+        this.meshes = []
+
+        for (let i = 0; i < BALL_COUNT; i++) {
+            this.meshes.push({
+                mesh: new THREE.Mesh(geometry1, wireframeMaterial),
+                heading: this.make_new_heading(),
+                rotation: this.make_new_heading().multiplyScalar(1/100)
+            })
+
+        }
+
+        for (let i = 0; i < SHINY_COUNT; i++) {
+            this.meshes.push({
+                mesh: new THREE.Mesh(geometry1, material),
+                heading: this.make_new_heading(),
+                rotation: this.make_new_heading().multiplyScalar(1/100)
+            })
+
+        }
+
+        for (let i = 0; i < this.meshes.length; i++) {
+            this.reset_mesh(i)
+
+        }
+
+        // BIG BOY (unused)
+        this.bgmesh = {
+            mesh: new THREE.Mesh(
+                new THREE.IcosahedronGeometry(1900, 1),
+                wireframeMaterial
+            ),
+            rotation: new THREE.Vector3(0.0001, 0, 0.0001),
+            heading: new THREE.Vector3(0, 0, 0)
+        }
+
+        for (let i in this.meshes) {
+            this.balls.add(this.meshes[i].mesh)
+        }
+        this.scene.add(this.balls)
+
+
+
+
+        // renderer
+        this.renderer = new THREE.WebGLRenderer( {
+            antialias: true,
+            canvas: this.canvas
+        } )
+        this.renderer.setPixelRatio( window.devicePixelRatio )
+        this.renderer.setSize( window.innerWidth, window.innerHeight )
+
+    }
+
+    render() {
+        
+        // this.balls.rotation.x += ( this.mouse.x - this.balls.rotation.x ) * CAMERA_MOVE_MODIFIER;
+        // this.balls.rotation.y += ( - this.mouse.y - this.balls.rotation.y ) * CAMERA_MOVE_MODIFIER;
+
+        // this.camera.lookAt( this.scene.position );
+        
+
+        this.renderer.render( this.scene, this.camera );
+    }
+
+    animate() {
+        requestAnimationFrame(this.animate.bind(this))
+        this.carton.animate()
+
+        const targetRotation = new THREE.Vector3(
+            this.mouse.x * CAMERA_MOVE_MODIFIER,
+            this.mouse.y * CAMERA_MOVE_MODIFIER,
+            0
+        )
+        const currentRotation = new THREE.Vector3(
+            this.balls.rotation.x,
+            this.balls.rotation.y,
+            this.balls.rotation.z
+        )
+
+        currentRotation.lerp(targetRotation, 0.5)
+
+        this.balls.rotation.x = currentRotation.x
+        this.balls.rotation.y = currentRotation.y
+        this.balls.rotation.z = currentRotation.z
+        // this.balls.rotation.y = (this.mouse.y) * CAMERA_MOVE_MODIFIER
+
+        for (let i = 0; i < this.meshes.length ; i++) {
+            this.meshes[i].mesh.position.add(this.meshes[i].heading)
+            this.meshes[i].mesh.rotation.x += this.meshes[i].rotation.x
+            this.meshes[i].mesh.rotation.y += this.meshes[i].rotation.y
+            this.meshes[i].mesh.rotation.z += this.meshes[i].rotation.z
+
+            if (this.meshes[i].mesh.position.distanceTo(this.meshes[i].target) < 100) {
+                this.reset_mesh(i)
+            }
+        }
+
+
+        this.render()
+
+    }
+
+    on_window_resize() {
+
+        this.windowHalfX = window.innerWidth / 2;
+        this.windowHalfY = window.innerHeight / 2;
+
+        this.camera.aspect = window.innerWidth / window.innerHeight;
+        this.camera.updateProjectionMatrix();
+
+        this.renderer.setSize( window.innerWidth, window.innerHeight );
+
+        this.carton.on_window_resize()
+    }
+
+    reset_mesh(index) {
+        const obj = this.meshes[index]
+
+        // place somewhere on the screen
+        obj.mesh.position.x = -400 - (Math.random() * 2400)
+        obj.mesh.position.y = -100 - (Math.random() * 700)
+        obj.mesh.position.z = -400 - (Math.random() * 2400)
+
+        // generate a random target
+        const target  = new THREE.Vector3(
+            600 + (Math.random() * 200),
+            200 + (Math.random() * 50),
+            600 + (Math.random() * 200),
+        )
+
+        obj.target = target.clone()
+        // create a heading
+        const heading = target.sub(obj.mesh.position).normalize().multiplyScalar(5)
+
+        obj.heading = heading
+
+        obj.lifetime = 0
+        obj.mesh.material.opacity = 1
+
+    }
+
+    make_new_heading() {
+        return new THREE.Vector3(
+            Math.random(),
+            Math.random(),
+            Math.random()
+        )
+    }
+
+
+    on_mouse_move(e) {
+        this.mouse.x = (e.clientX / window.innerWidth)
+        this.mouse.y = (e.clientY / window.innerHeight)
+        this.carton.set_mouse(this.mouse)
+    }
+}
+
+
+class CartonDriver {
+    constructor() {
+
+        this.canvas = document.querySelector('canvas#carton')
+        const rect = this.canvas.getBoundingClientRect()
+        // box art
+        const carton_geometry = new THREE.BoxGeometry(13,20,7);
+        // const wireframeMaterial = new THREE.MeshBasicMaterial( { color: 0x000000, wireframe: true, transparent: true } );
+        const basic_material = new THREE.MeshPhongMaterial({color: 0xbacdb0, roughness: 0.5 })
+        
+        this.carton = new THREE.Mesh(carton_geometry, basic_material);
+        this.carton.position.z = 0
+        this.carton.position.x = 0
+
+        this.scene = new THREE.Scene()
+        this.scene.add(this.carton)
+
+        const light = new THREE.DirectionalLight( 0xffffff );
+        light.position.set( 0, 0, 1 );
+        this.scene.add( light );
+
+        this.camera = new THREE.PerspectiveCamera( 20, rect.width / rect.height, 1, 10000 )
+        this.camera.position.z = 100
+
+        this.mouse = {x: 0, y: 0}
+
+        // renderer
+        this.renderer = new THREE.WebGLRenderer( {
+            antialias: true,
+            alpha: true,
+            canvas: this.canvas
+        } )
+        this.renderer.setClearColor( 0x000000, 0 ) // the default
+        this.scene.background = null
+        this.renderer.setPixelRatio( window.devicePixelRatio )
+        this.renderer.setSize( rect.width, rect.height)
+    }
+
+    set_mouse(mouse) {
+        this.mouse = mouse
+    }
+
+    on_window_resize() {
+
+        this.camera.aspect = window.innerWidth / window.innerHeight;
+        this.camera.updateProjectionMatrix();
+
+        const rect = this.canvas.getBoundingClientRect()
+
+        this.renderer.setSize( rect.width, rect.height );
+
+    }
+    render() {
+        this.renderer.render( this.scene, this.camera );
+
+    }
+
+    animate() {
+        this.carton.rotation.x = this.mouse.y * CARTON_MOVE_MODIFIER
+        this.carton.rotation.y = this.mouse.x * CARTON_MOVE_MODIFIER
+        this.render()
+    }
+
+}
+ 
+window.onload = () => {
+    window.cd = new CanvasDriver()
+    window.cd.animate()
+
+    window.onresize = () => cd.on_window_resize()
+    window.addEventListener('mousemove', e => cd.on_mouse_move(e))
+}
+
